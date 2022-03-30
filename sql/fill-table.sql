df297917ac22e1cd5b80c4ebaa41322d246692db
Khai Fahmi Zaki <khai@accesstel.com.au>
2022-03-30 20:21:46 +1100

    docker-compose to run postgres, pgadmin and metabase
    DB is pointed to hike, created a  table and auto fill with data

---
+++ sql/fill-table.sql
@@ -0,0 +1,25 @@
+INSERT INTO records (
+  title,
+  description,
+  location,
+  state,
+  lat,
+  long
+) VALUES (
+  'example title 1',
+  'example description 1',
+  'example location 1',
+  'VIC',
+  '90',
+  '132'
+);
+
+INSERT INTO records (
+  title,
+  lat,
+  long
+) VALUES (
+  'example title 2',
+  78,
+  132.11
+);
